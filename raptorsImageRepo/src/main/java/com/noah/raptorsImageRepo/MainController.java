dc8383d6e228b88e7eeb1f62aec21e8f00e4bd9f
Noah <Noah@192.168.2.78>
2020-02-05 13:26:11 -0500

    Add Thymeleaf routing back to index page

--- raptorsImageRepo/src/main/java/com/noah/raptorsImageRepo/MainController.java
+++ raptorsImageRepo/src/main/java/com/noah/raptorsImageRepo/MainController.java
@@ -1,26 +1,30 @@
 package com.noah.raptorsImageRepo;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.noah.raptorsImageRepo.dto.UploadImage;
 import com.noah.raptorsImageRepo.service.IRaptorsImageRepoService;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class MainController {
 	
 	protected static final Logger log = LogManager.getLogger(MainController.class);
 	
 	@Autowired
 	private IRaptorsImageRepoService raptorsImageRepoService;
+	
+	@RequestMapping("/index") 
+	public String index() {
+		// Returns index.html via thymeleaf
+		return "index"; 
+	}
 
 	@RequestMapping(method=RequestMethod.POST, value="/upload-image")
 //	public void uploadImage(@RequestBody UploadImage uploadImage) {
