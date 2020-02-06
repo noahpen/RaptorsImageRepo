ea86f3e430146f712c60e1aaeaedbc27309aac60
Noah <Noah@192.168.2.78>
2020-02-05 22:50:51 -0500

    Add test class

--- raptorsImageRepo/src/test/java/com/noah/raptorsImageRepo/RaptorsImageRepoApplicationTests.java
+++ raptorsImageRepo/src/test/java/com/noah/raptorsImageRepo/RaptorsImageRepoApplicationTests.java
@@ -1,13 +1,32 @@
 package com.noah.raptorsImageRepo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.noah.raptorsImageRepo.dao.IImageDAO;
+import com.noah.raptorsImageRepo.dto.ImageDTO;
+import com.noah.raptorsImageRepo.service.IImageService;
+
 
 @SpringBootTest
 class RaptorsImageRepoApplicationTests {
 
+	@Autowired
+	private IImageService imageService;
+	private ImageDTO imageDTO;
+	
+	@MockBean
+	private IImageDAO imageDAO;
+	
 	@Test
-	void contextLoads() {
+	private void testAddToDB() throws Exception {
+		imageDTO.setPath("/example/path/here");
+		imageDTO.setFileName("lowry.jpg");
+		imageDTO.setCaption("Raptors Point guard");
+		imageDTO.setImageId(1);
+		imageService.saveImage(null, imageDTO);
 	}
 
 }
